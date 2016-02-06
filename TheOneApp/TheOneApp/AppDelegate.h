d18d76b418a6cbe82e0a0584c0d012cf7928f0f2
xiaoxuejing <792226310@qq.com>
2016-02-06 23:08:53 +0800

    创建项目

---
+++ TheOneApp/TheOneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheOneApp
+//
+//  Created by 曾一桀 on 16/2/6.
+//  Copyright © 2016年 JackieZeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheOneApp/TheOneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheOneApp
+//
+//  Created by 曾一桀 on 16/2/6.
+//  Copyright © 2016年 JackieZeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
